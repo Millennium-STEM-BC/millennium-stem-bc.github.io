e1dde9529d29ca384fe401450492be3536cd7a48
Jerry Hu <work.jerrywu@gmail.com>
2024-04-13 19:00:40 -0700

    added firebase configs

---
+++ assets/js/firebase_init.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAQ5WwPNH-Bbu5X3_n_LOg-uA3hj3xqe9U",
+    authDomain: "millenniumstem-f2285.firebaseapp.com",
+    projectId: "millenniumstem-f2285",
+    storageBucket: "millenniumstem-f2285.appspot.com"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
